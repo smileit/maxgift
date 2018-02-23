444255b860b3d793a91e35080161e404d605972a
admin <admin@admindeMacBook-Pro.local>
2018-02-23 15:13:43 +0800

    save

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import { NavLink as Link } from 'react-router-dom'
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className='main-cl'>
         Home page
         <Link className='link-btn btn'
           to='/p1'
